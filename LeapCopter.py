6510ec9707a01a3f0db3341ff125625748b23199
Evan Ye <applepie31415@gmail.com>
2013-07-25 10:46:20 -0700

    new Leap python code for newer Leap veresion

--- LeapCopter.py
+++ LeapCopter.py
@@ -1,4 +1,3 @@
-import _LeapPython
 import Leap, sys, math
 import serial
 import time
@@ -12,131 +11,145 @@ def convert(angle):
   if(angle > 180):
     return 127 - ((360 - angle)*127)/45
 
-
-
-
   
 class SampleListener(Leap.Listener):
   
-  def onInit(self, controller):
+  def on_init(self, controller):
     print "Initialized"
     self.pitch_offset, self.roll_offset = 0, 0
-    self.tool_was_present = False
+    # self.tool_was_present = False
 
-  def onConnect(self, controller):
+  def on_connect(self, controller):
     print "Connected"
 
-  def onDisconnect(self, controller):
+  def on_disconnect(self, controller):
     print "Disconnected"
 
-  def onFrame(self, controller):
-        
+  def on_frame(self, controller):
     frame = controller.frame()
-    hands = frame.hands()
-    try:
-        if len(hands) == 0 and self.tool_was_present:
-            # Toggle heli lights off
-            ser.write('bbb')
-            print('tool disappeared')
-            self.tool_was_present = False
+
+    # if len(hands) == 0 and self.tool_was_present:
+    #     # Toggle heli lights off
+    #     ser.write('bbb')
+    #     print('tool disappeared')
+    #     self.tool_was_present = False
+        
+    if not frame.hands.empty:
+        # Get the first hand
+        hand = frame.hands[0]
+
+        height = hand.palm_position.y
+        
+        # Get the hand's normal vector and direction
+        normal = hand.palm_normal
+        direction = hand.direction
+
+        pitch = direction.pitch * Leap.RAD_TO_DEG
+        roll = normal.roll * Leap.RAD_TO_DEG
           
-        if len(hands) > 0:
-            hand = hands[0]
-            palmRay = hand.palm()
-            fingers = hand.fingers()
-              
-            tool_present = False
+        # tool_present = False
 
-            #check if a tool is present
-            for hand in hands:
-              for finger in hand.fingers():
-                if finger.isTool():
-                  tool_present = True
+        #check if a tool is present
+        # for hand in hands:
+        #   for finger in hand.fingers():
+        #     if finger.isTool():
+        #       print 'tool'
+        #       tool_present = True
 
-            if not self.tool_was_present and tool_present:
-              # Toggle heli lights on
-              ser.write('bbb')
-              print('tool initially found')
-              self.tool_was_present = True
-              
-            if self.tool_was_present and not tool_present:
-              # Toggle heli lights off
-              ser.write('bbb')
-              print('tool disappeared')
-              self.tool_was_present = False
+        # if not self.tool_was_present and tool_present:
+        #   # Toggle heli lights on
+        #   ser.write('bbb')
+        #   print('tool initially found')
+        #   self.tool_was_present = True
+          
+        # if self.tool_was_present and not tool_present:
+        #   # Toggle heli lights off
+        #   ser.write('bbb')
+        #   print('tool disappeared')
+        #   self.tool_was_present = False
 
-                  
-            palm = palmRay.position
-            normal = hand.normal()
-            if normal is not None:
-                # pitch (front/back) and roll (left/right)
-                pitchAngle = math.atan2(normal.z, normal.y) * 180/math.pi + 180
-                rollAngle = math.atan2(normal.x, normal.y) * 180/math.pi + 180
+        #Calibration - saves offset when 2 hands are present
+        # if len(hands) > 1:
+        #     self.pitch_offset = int(convert(pitchAngle) - 127)
+        #     self.roll_offset = int(convert(rollAngle) - 127)     
+        
+        throttle_data = int((((height-30)*255)/300))
+        pitch_data = int(255 - (convert(pitch)+self.pitch_offset))
+        roll_data = int(convert(roll)) + self.roll_offset
 
-            #Calibration - saves offset when 2 hands are present
-            if len(hands) > 1:
-                self.pitch_offset = int(convert(pitchAngle) - 127)
-                self.roll_offset = int(convert(rollAngle) - 127)     
+        #normalize throttle
+        if throttle_data < 0:
+          throttle_data = 0
+        elif throttle_data > 240:
+          throttle_data = 240
 
-      
-            
+        #Map to polynomial scale
+        #More control for lower values
+        throttle_data = int(((throttle_data/2)**2)/63.25)
+        
+        #normalize pitch
+        if pitch_data > 255:
+          pitch_data = 255
+        elif 90 < pitch_data and pitch_data < 190: #threshold
+          pitch_data = 122
+        elif pitch_data < 0:
+          pitch_data = 0
 
-            throttle_data = int((((palm.y-30)*255)/300))
-            pitch_data = int(255 - (convert(pitchAngle)+self.pitch_offset))
-            roll_data = int(convert(rollAngle)) + self.roll_offset
-
-            if throttle_data > 230:
-              throttle_data = 230
-            if pitch_data > 255:
-              pitch_data = 255              
-            if roll_data > 255:
-              roll_data = 255
-
-            if throttle_data < 0:
-              throttle_data = 0
-            if pitch_data < 0:
-              pitch_data = 0
-            if roll_data < 0:
-              roll_data = 0
-              
-            #Map to polynomial scale
-            #More control for lower values
-            throttle_data = int(((throttle_data/2)**2)/63.25)
-            
-            #data tag; arduino reads the followng three values as throttle, pitch and roll
-            #ensures ardiuno read won't fall out of sync
-            ser.write('a')
-            ser.write('a')
-            ser.write('a')
-
-            #write data to arduino
-            ser.write(chr(throttle_data))
-            ser.write(chr(pitch_data))
-            ser.write(chr(roll_data))
-            
-            # for debugging
-            print throttle_data , pitch_data , roll_data , len(hands), 'pitch_offset:', self.pitch_offset, 'roll_offset:', self.roll_offset
+        #normalize roll
+        if roll_data > 255:
+          roll_data = 255
+        elif 80 < roll_data and roll_data < 150: # threshhold
+          roll_data = 122
+        elif roll_data < 0:
+          roll_data = 0
           
         
+        #data tag; arduino reads the followng three values as throttle, pitch and roll
+        #ensures ardiuno read won't fall out of sync
+        ser.write('a')
+        ser.write('a')
+        ser.write('a')
 
-        #delay before sending new data
-        time.sleep(.025)
-    except Exception as e:
-        print(e)
+        #write data to arduino
+        ser.write(chr(throttle_data))
+        ser.write(chr(pitch_data))
+        ser.write(chr(roll_data))
         
+        # for debugging
+        print 'throttle: {0}, pitch: {1}, roll: {2}'.format(throttle_data, pitch_data, roll_data)
+        #print 'pitch_offset:', self.pitch_offset, 'roll_offset:', self.roll_offset
+       
+    #delay before sending new data
+    time.sleep(.025)
     
-    
-ser = serial.Serial('COM14',115200)
-time.sleep(5)
-listener = SampleListener()
-controller = Leap.Controller(listener)
+ser = serial.Serial('/dev/tty.usbserial-A900acS7',115200)
+time.sleep(2)
 
-# press enter when no hands are present to kill throttle
-# while True:
-#     val = sys.stdin.readline()
-#     ser.write('a')
-#     ser.write('a')
-#     ser.write('a')
-#     ser.write(chr(0))
-#     ser.write(chr(127))
-#     ser.write(chr(127))
+def main():  
+  # Create a sample listener and controller
+  listener = SampleListener()
+  controller = Leap.Controller()
+
+  # Have the sample listener receive events from the controller
+  controller.add_listener(listener)
+
+  # Keep this process running until Enter is pressed
+  print "Press Enter to quit..."
+  sys.stdin.readline()
+
+  # Remove the sample listener when done
+  controller.remove_listener(listener)
+
+
+  # press enter when no hands are present to kill throttle
+  # while True:
+  #     val = sys.stdin.readline()
+  #     print val
+  #     ser.write('a')
+  #     ser.write('a')
+  #     ser.write('a')
+  #     ser.write(chr(0))
+  #     ser.write(chr(127))
+  #     ser.write(chr(127))
+if __name__ == "__main__":
+    main()   
